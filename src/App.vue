cc179cc0fd570e78bc0f38faad1620dac097c4cb
bensenplus <12292394@qq.com>
2023-05-30 14:58:07 +0800

    切换 暗黑/明亮模式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <el-container>
       <el-header height="30px">
-        
+        <el-switch v-model="dark" @change="changeMode()" active-text="暗黑" inactive-text="明亮" inline-prompt size="large" style="position: absolute; right:10px;"/>
       </el-header>
       <el-container>
         <el-aside width="150px">
@@ -17,23 +17,35 @@
 html, body, #app{
   margin: 0; 
   padding: 0;
-  width: 100%;
-}
-::-webkit-scrollbar {
-    width: 6px;
-    height: 6px;
 }
 
+::-webkit-scrollbar {
+    width: 10px;
+    height: 10px;
+} 
+
 ::-webkit-scrollbar-track {
-    background-color: transparent;
+    background-color: #000;
 }
 
 ::-webkit-scrollbar-thumb {
     border-radius: 5px;
     box-shadow: inset 0 0 0px rgba(240, 240, 240, 0.5);
-    background-color: rgba(100, 100, 100, 0.5);
+    background-color: rgba(100, 100, 100, 1);
 }
 </style>
 <script setup>
   import Menu from './components/Menu.vue';
+  import { ref } from 'vue'
+
+  const dark = ref(true)
+
+  const changeMode = () => {
+      if(dark.value){
+        document.documentElement.setAttribute('class','dark')
+      }else{
+        document.documentElement.setAttribute('class','light')
+      }
+  }
+
 </script>
